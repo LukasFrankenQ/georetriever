ca34d50d4112f06d1cd4a212ad16cb27c825f2f3
Lukas Franken <lukas.b.franken@gmail.com>
2022-11-10 13:26:37 +0000

    added first test

--- georetriever/__init__.py
+++ georetriever/__init__.py
@@ -1,3 +1,1 @@
 __version__ = "0.0.3"
-
-from geo_retriever.retriever import GeoRetriever
---
+++ test/test_lith.py
@@ -0,0 +1,16 @@
+import pytest
+
+from georetriever.utils import Lith
+from georetriever.utils.geo_utils import get_random_lith
+
+
+def test_lith_conversion():
+    lith_obj = get_random_lith()
+    lith_list = lith_obj.tolist()
+    lith_obj = Lith.from_list(lith_list)
+
+    assert lith_list == lith_obj.tolist()
+
+
+if __name__ == "__main__":
+    test_lith_conversion()
