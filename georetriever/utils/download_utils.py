ce70c4d25e7284140857f86dd5bc185e22e9961d
LukasFrankenQ <lukas.b.franken@gmail.com>
2023-01-24 23:08:00 +0000

    added maybe download watershed func

--- georetriever/utils/download_utils.py
+++ georetriever/utils/download_utils.py
@@ -1,7 +1,8 @@
 import wget
 import os
-from pathlib import Path
 import urllib.request
+from wsl_pathlib.path import WslPath as Path
+from zipfile import ZipFile
 
 
 def maybe_download(filepath, url):
@@ -16,7 +17,7 @@ def maybe_download(filepath, url):
     """
 
     assert (
-        "watershed" in url
+        "watershed" not in url
     ), "Use maybe_download_watershed() to download watershed data"
 
     if not os.path.exists(filepath):
@@ -26,7 +27,24 @@ def maybe_download(filepath, url):
         wget.download(url, out=filepath)
 
 
-def maybe_download_watershed():
+def maybe_download_watershed(datapath, url):
+    """
+    Checks if file exists.
+
+    if not downloads it. Watershed download origin requires
+    specific header and hence warrants a separate function
+
+    Args:
+        datapath(str): path to dir (or where it will be stored)
+        url(str): download link
+    """
+
+    if os.path.exists(datapath) and len(os.listdir(datapath)) > 0:
+        return
+
+    datapath = Path(datapath)
+
+    datapath.parent.mkdir(parents=True, exist_ok=True)
 
     opener = urllib.request.URLopener()
     opener.addheader(
@@ -34,6 +52,13 @@ def maybe_download_watershed():
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
     )
 
+    zip_path = Path(str(datapath) + ".zip")
+
+    opener.retrieve(url, zip_path)
+
+    with ZipFile(zip_path, "r") as zip:
+        zip.extractall(path=datapath)
+
 
 if __name__ == "__main__":
-    maybe_download()
+    pass
